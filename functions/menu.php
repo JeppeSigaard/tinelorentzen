ef39bdf4ead28a55599d5ab73185fddb04215e9c
JeppeSigaard <jeppe@smatmonkey.dk>
2015-06-10 12:12:09 +0200

    oprettet ny menu til forsiden

--- functions/menu.php
+++ functions/menu.php
@@ -1,6 +1,7 @@
 <?php
 register_nav_menus( array(
 	'main_menu' => 'Hamburgermenu',
+	'front_menu' => 'Menu i forsidens kasser',
 ) );
 
 ?>
--- libs/modules/front-links.php
+++ libs/modules/front-links.php
@@ -1,12 +1,5 @@
 <?php
 
-$args = array(
-    'post_type' => 'post',
-    'orderby'   => 'menu_order',
-    'hide_empty'    => 0,
-    'exclude'   => 1,
-);
-$categories = get_categories( $args );
 
 ?>
    
@@ -19,13 +12,6 @@ $categories = get_categories( $args );
         <a href="<?php bloginfo('url') ?>/erhverv"><span>Erhverv</span></a>
     </div>
     <div style="background-image:url(<?php echo get_template_directory_uri() ?>/libs/img/sidstenyt.jpg);">
-        <a class="small" href="#seneste-nyt">Sidste nyt på siden</a>
-        <ul>
-            <?php foreach($categories as $cat):?>
-            <li>
-                <a href="<?php echo get_category_link($cat->term_id) ?>"><?php echo $cat->name ?></a>
-            </li>
-           <?php endforeach; ?>
-        </ul>
+        <?php wp_nav_menu(array('theme_location' => 'front_menu', 'container' => false, 'fallback_cb' => '')); ?>
     </div>
 </section>
